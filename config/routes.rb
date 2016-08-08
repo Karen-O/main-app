c1f42cd0ca3865f6e355da21f2bb199a232c204c
Karen E. Ochsenreiter <karen.e.och@gmail.com>
2016-08-07 21:35:38 -0400

    Define association between models; create authenticated user via client-side sign-up/login.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  root 'home#index'
+  
+  devise_for :users
 
   resources :posts
   
-  devise_for :users
+#  root 'home#index'
 
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
